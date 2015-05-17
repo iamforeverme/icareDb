0b67e2f3997c9b574879914c41bf4fafc450c201
iamforeverme <iamforeverme@163.com>
2015-05-17 16:55:30 +0800

    new generator

---
+++ sqlDatabase/clearData.sql
@@ -0,0 +1,8 @@
+use icare;
+
+delete from signal_type where id>0; 
+delete from protege where id>0;
+delete from quick_solution where id>0;
+delete from setting where id>0;
+delete from warning_type where id>0;
+delete from device where id>0;
---
+++ sqlDatabase/generateTestData.sql
@@ -0,0 +1,9 @@
+insert into device(mac) values("12331");
+insert into device(mac) values("1234");
+insert into device(mac) values("qweasd");
+insert into device(mac) values("23eddq");
+insert into device(mac) values("fgher3");
+
+insert into protege(name,id_card_num,telphone,mobile,contacts_name1,contacts_tel1,contacts_name2,contacts_tel2,history,province,city,address,photo_dir,age,monitoring_level) values("张大爷","150204193206052290","027-31876542","13942365587","张小胖","13942365589","张小芳","13942365592","高血压,心脏病","上海","上海","陆家嘴张家大院","",86,"level1");
+
+insert into protege(name,id_card_num,telphone,mobile,contacts_name1,contacts_tel1,contacts_name2,contacts_tel2,history,province,city,address,photo_dir,age,monitoring_level) values("王大爷","1110332193206052290","010-31239542","13942365587","王二","13942365590","王初花","13942365664","糖尿病","北京","北京","北新桥","",88,"level2");
