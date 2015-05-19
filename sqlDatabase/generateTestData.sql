8f6ab2404dc6af8939adcc03fa583352b57101b4
iamforeverme <iamforeverme@163.com>
2015-05-19 23:02:06 +0800

    new row

--- sqlDatabase/generateTestData.sql
+++ sqlDatabase/generateTestData.sql
@@ -7,3 +7,9 @@ insert into device(mac) values("fgher3");
 insert into protege(name,id_card_num,telphone,mobile,contacts_name1,contacts_tel1,contacts_name2,contacts_tel2,history,province,city,address,photo_dir,age,monitoring_level) values("张大爷","150204193206052290","027-31876542","13942365587","张小胖","13942365589","张小芳","13942365592","高血压,心脏病","上海","上海","陆家嘴张家大院","",86,"level1");
 
 insert into protege(name,id_card_num,telphone,mobile,contacts_name1,contacts_tel1,contacts_name2,contacts_tel2,history,province,city,address,photo_dir,age,monitoring_level) values("王大爷","1110332193206052290","010-31239542","13942365587","王二","13942365590","王初花","13942365664","糖尿病","北京","北京","北新桥","",88,"level2");
+
+insert into signal_type(type) values("WET");
+
+insert into warning_type(type) values("LONGLEAVE");
+
+insert into monitor_data(protege_id,signal_type_id,rec_time) values(1,1,'2009-9-9 23:22:11');
